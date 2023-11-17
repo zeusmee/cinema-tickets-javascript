2d92db0bd85cc2485a0041443b6c0e8de646498e
zeusmee <sm.meenss@gmail.com>
2023-11-17 11:38:47 +0000

    mocha config fix

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,20 +1,6 @@
-{
-  "name": "cinema-tickets-javascript",
-  "description": "This is a coding exercise which will allow you to demonstrate how you code and your approach to a given problem.",
-  "version": "1.0.1",
-  "engines": {
-    "node": ">=16.15.1"
-  },
-  "type": "module",
-  "scripts": {
-    "test": "mocha --config .mocharc.cjs"
-  },
-  "devDependencies": {
-    "jest": "28.1.1",
-    "mocha": "^10.0.0",
-    "sinon": "^17.0.1"
-  },
-  "dependencies": {
-    "esm": "^3.2.25"
-  }
-}
+module.exports = {
+  spec: 'test/ticketService.test.js',
+  recursive: true,
+  timeout: 5000, 
+  reporter: 'spec' 
+};
